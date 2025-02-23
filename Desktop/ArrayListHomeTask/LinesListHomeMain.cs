b8c0006a80a97ce4258b9c8d40e9758da2ebe5b6
Sss <surkun1992@gmail.com>
2025-02-23 18:57:16 +0700

    Времменно добавлен!

---
+++ Desktop/ArrayListHomeTask/LinesListHomeMain.cs
@@ -0,0 +1,38 @@
+﻿namespace LinesListHomeTask;
+
+internal class LinesListHomeMain
+{
+    public static List<string> GetLinesListFromFile(string inputFilePath)
+    {
+        using StreamReader reader = new StreamReader(inputFilePath);
+
+        List<string> linesList = new List<string>();
+
+        string? currentLine;
+
+        while ((currentLine = reader.ReadLine()) != null)
+        {
+            linesList.Add(currentLine);
+        }
+
+        return linesList;
+    }
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            List<string> linesList = GetLinesListFromFile("..\\..\\..\\TextFile\\text.txt");
+
+            Console.WriteLine(string.Join(Environment.NewLine, linesList));
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Файл не найден");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Не найден путь к файлу");
+        }
+    }
+}
